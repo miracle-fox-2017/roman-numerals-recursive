77527c8c94ff2ae6cb258c1fa3debc8ccf687527
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-16 18:35:15 +0700

    roman recursive done

--- roman_recursive.js
+++ roman_recursive.js
@@ -1,5 +1,30 @@
 function to_roman (num) {
   // your implementation code here
+  let output = ''
+  let roman = {
+  	M: 1000, 
+  	CM: 900, 
+  	D: 500, 
+  	CD: 400,
+  	C: 100,
+  	XC: 90,
+  	L: 50,
+  	XL: 40,
+  	X: 10,
+  	IX: 9,
+  	V: 5,
+  	IV: 4,
+  	I: 1
+  }
+
+  for (let key in roman) {
+  	if (num === 0) {
+  	  return output
+  	} else if (num >= roman[key]) {
+  	  output += key
+  	  return output + to_roman(num -= roman[key]);
+  	}
+  }
 }
 
 // Drive code
