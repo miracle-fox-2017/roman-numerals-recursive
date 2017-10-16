9d218bf2a72de218c6dc981dfa6e72d1b4cecf92
Ayunda <ayundahamzah@gmail.com>
2017-10-16 18:04:52 +0700

    ayundahamzah

--- roman_recursive.js
+++ roman_recursive.js
@@ -1,6 +1,21 @@
 function to_roman (num) {
-  // your implementation code here
+  var arabic = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  var roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+  var hasil = ""; 
+ if(num===0){
+      return hasil;
+    }
+    else{
+      for(i = 0; i < arabic.length; i++) {
+        while(num>=arabic[i]){
+        hasil+=roman[i]
+        num-=arabic[i]
+        return hasil+= to_roman(num);
+        }
+    }
+  
 }
+
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
@@ -11,3 +26,19 @@ console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
+
+
+// function to_roman (num) {
+//   var arabic = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+//   var roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+//   var hasil = "";
+//   for(i = 0; i < arabic.length; i++) {
+//   // console.log('masuk for 1')
+//     while(num>=arabic[i]){
+//       hasil+=roman[i]
+//       num-=arabic[i]
+//     }
+//   }
+// return hasil;
+// }
+  
